d3c3d45678b34ed8c46d4cc47adb6c1fcd11f0b5
Misyk8925 <129992573+Misyk8925@users.noreply.github.com>
2025-06-17 21:28:22 +0200

    add configuration management and HTTP server setup

---
+++ config/internal.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" emv-default:"storage/mySql.go"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:":8000"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"5s"`
+}
+
+func MustLoadConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		panic("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("CONFIG_PATH does not exist")
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	fmt.Printf("Config: %+v\n", config)
+
+	return &config
+}
